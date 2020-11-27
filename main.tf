31413bce045bd062ad5b583dde1d8b2552a3e785
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2020-11-27 13:54:27 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,23 +4,24 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "vmsspoc-rg2"
+  name     = "vmsspocimage-rg"
   location = "Southeast Asia"
 }
 
 module "linuxservers" {
   source                        = "./terraform-azurerm-compute"
   resource_group_name           = azurerm_resource_group.example.name
-  vm_hostname                   = "vmsspocrhelvm1"
+  vm_hostname                   = "muravmssimagevm1"
   nb_public_ip                  = 0
   remote_port                   = "22"
-  vm_os_id                      = data.azurerm_shared_image.redhat.id
+  nb_instances                  = 1
+  vm_os_id                      = data.azurerm_image.redhat.id
   vm_os_publisher               = "RedHat"
   vm_os_offer                   = "RHEL"
   vm_os_sku                     = "7.8"
   vnet_subnet_id                = data.azurerm_subnet.appsubnet.id
-  boot_diagnostics              = true
-  delete_os_disk_on_termination = true
+  boot_diagnostics              = false
+  delete_os_disk_on_termination = false
   nb_data_disk                  = 1
   data_disk_size_gb             = 64
   data_sa_type                  = "Premium_LRS"
@@ -28,8 +29,10 @@ module "linuxservers" {
   vm_size                       = "Standard_D4s_v3"
   
   tags = {
-    environment = "dev"
-    costcenter  = "it"
+    environment = "poc"
+    costcenter  = "testpoc"
+    application = "testpoc"
+    owner       = "Mura"
   }
 
   enable_accelerated_networking = true
@@ -38,6 +41,7 @@ module "linuxservers" {
 output "linux_vm_private_ips" {
   value = module.linuxservers.network_interface_private_ip
 }
+
 
 output "linux_vm_hostname" {
   value = module.linuxservers.vm_hostname
